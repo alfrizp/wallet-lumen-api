4fdaaf3bf3daa744df4b8ff5d7f78f6273d100c2
alip <alifrizkipambudi@gmail.com>
2019-02-09 19:07:32 +0700

    create transaction model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'date', 'amount', 'in_out', 'description',
+        'category_id', 'user_id',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getTypeAttribute()
+    {
+        return $this->in_out ? 'income' : 'outcome';
+    }
+
+    public function getDateOnlyAttribute()
+    {
+        return substr($this->date, -2);
+    }
+
+    public function getMonthAttribute()
+    {
+        return Carbon::parse($this->date)->format('m');
+    }
+
+    public function getYearAttribute()
+    {
+        return Carbon::parse($this->date)->format('Y');
+    }
+
+    public function getAmountStringAttribute()
+    {
+        $amountString = number_format($this->amount, 2);
+
+        if ($this->in_out == 0) {
+            $amountString = '- '.$amountString;
+        }
+
+        return $amountString;
+    }
+}
