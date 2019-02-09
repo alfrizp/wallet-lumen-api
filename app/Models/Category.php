25999b65aeea700617c6151f249dc1362563942a
alip <alifrizkipambudi@gmail.com>
2019-02-09 19:07:01 +0700

    create category model & category transformer

---
+++ app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Transformers\CategoryTransformer;
+
+class Category extends Model {
+    use SoftDeletes;
+
+    protected $table = 'categories';
+    protected $dates = ['deleted_at'];
+    public $transformer = CategoryTransformer::class;
+
+    protected $fillable = [
+        'name', 'description', 'color',
+        'user_id'
+    ];
+
+    public function getRouteKeyName() {
+        return 'id';
+    }
+
+    public function creator() {
+        return $this->belongsTo(User::class);
+    }
+}
