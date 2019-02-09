75054d429c4ff65b9fe79dabfaad54056934eefc
alip <alifrizkipambudi@gmail.com>
2019-02-09 18:58:23 +0700

    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CategoryPolicy {
+
+    use HandlesAuthorization;
+
+    public function viewCategory($user, Category $category) {
+        // return true;
+        return $user->id === $category->user_id;
+    }
+
+    public function update(User $user, Category $category) {
+        // Update $user authorization to update $category here.
+        return $user->id == $category->user_id;
+    }
+
+    public function delete(User $user, Category $category) {
+        // Update $user authorization to delete $category here.
+        return $user->id == $category->user_id && $category->transactions->isEmpty();
+    }
+}
