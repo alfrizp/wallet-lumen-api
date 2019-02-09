42d4123ca6265abac838b95d8873a5328f5f7054
alip <alifrizkipambudi@gmail.com>
2019-02-09 19:15:26 +0700

    [LOGIN] - create login user module

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+// use Illuminate\Http\Request;
+use Pearl\RequestValidate\RequestAbstract;
+
+class LoginRequest extends RequestAbstract {
+    public function rules() {
+        return [
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ];
+    }
+}
