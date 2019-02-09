7d610793a6759dec8cbea43736d67ae75cf562a0
alip <alifrizkipambudi@gmail.com>
2019-02-09 19:14:21 +0700

    [REGISTER] - create register user module

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\ApiController;
+use App\Http\Requests\Auth\RegisterRequest;
+
+class RegisterController extends ApiController {
+    public function register(RegisterRequest $registerRequest) {
+        $registerRequest['password'] = bcrypt($registerRequest['password']);
+        User::create($registerRequest->all());
+
+        return $this->showMessage(__('auth.register.success'));
+    }
+}
---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Pearl\RequestValidate\RequestAbstract;
+
+class RegisterRequest extends RequestAbstract {
+    public function rules() {
+        return [
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+}
