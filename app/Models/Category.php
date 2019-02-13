5757e5918f357654d3cd9a9d3c02adc526ddbff8
alip <alifrizkipambudi@gmail.com>
2019-02-13 15:10:25 +0700

    [CATEGORY] - remove category transformer

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Transformers\CategoryTransformer;
 
 class Category extends Model
 {
@@ -12,7 +11,6 @@ class Category extends Model
 
     protected $table = 'categories';
     protected $dates = ['deleted_at'];
-    public $transformer = CategoryTransformer::class;
 
     protected $fillable = [
         'name', 'description', 'color',
