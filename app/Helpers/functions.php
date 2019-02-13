d0c57b9ab46fa91bd7b024ecf4c79e71bd14bb89
alip <alifrizkipambudi@gmail.com>
2019-02-13 14:59:58 +0700

    create balance and format_number helper functions

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -37,3 +37,33 @@ if (! function_exists('bcrypt')) {
         return app('hash')->driver('bcrypt')->make($value, $options);
     }
 }
+
+/**
+ * Get balance amount based on transactions.
+ *
+ * @param  string|null  $perDate
+ * @param  string|null  $startDate
+ * @return float
+ */
+function balance($perDate = null, $startDate = null)
+{
+    $transactionQuery = DB::table('transactions');
+    if ($perDate) {
+        $transactionQuery->where('date', '<=', $perDate);
+    }
+    if ($startDate) {
+        $transactionQuery->where('date', '>=', $startDate);
+    }
+    $transactions = $transactionQuery->where('user_id', request()->user()->getKey())->get();
+
+    return $transactions->sum(function ($transaction) {
+        return $transaction->in_out ? $transaction->amount : -$transaction->amount;
+    });
+}
+
+function format_number(float $number)
+{
+    $number = number_format($number, 2);
+
+    return str_replace('-', '- ', $number);
+}
